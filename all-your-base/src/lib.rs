8e5929a804e0da1eeb3d0041b16fbd0aa73d167e
Nima155 <nimashadab@hotmail.co.uk>
2021-08-06 11:25:13 +0430

    ...

--- all-your-base/src/lib.rs
+++ all-your-base/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidInputBase,
@@ -36,11 +38,66 @@ pub enum Error {
 ///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
 ///    However, your function must be able to process input with leading 0 digits.
 ///
+// let input_base = 97;
+// let input_digits = &[3, 46, 60];
+// let output_base = 73;
+// 6, 10, 45
+/*
+
+    3 46 60 = 60 + 46 * 2 + 3 * 8 =
+
+    10 Base 10
+    10 %= 2            0
+    10 /= 2
+    5  %= 2            1
+    2 %= 2             0
+    1 %= 2             1
+    0                  0
+
+    01010 => 10 in Base 2
+*/
+
+/*
+    [1, 0]
+
+
+
+
+*/
+
+fn convert_to_base10(number: &[u32], base: u32) -> u32 {
+    number
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (i, x)| acc + x * base.pow(i as u32))
+}
+
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    unimplemented!(
-        "Convert {:?} from base {} to base {}",
-        number,
-        from_base,
-        to_base
-    )
+    
+    match (from_base, to_base) {
+        (0, _) | (1, _) => return Err(Error::InvalidInputBase),
+        (_, 0) | (_, 1) => return Err(Error::InvalidOutputBase),
+        _ => {}
+    }
+
+    if let Some(n) = number.iter().find(|f| **f >= from_base) {
+        return Err(Error::InvalidDigit(*n));
+    }
+
+    let mut in_10 = convert_to_base10(number, from_base);
+
+    let mut ans = VecDeque::new();
+
+    while in_10 > 0 {
+        let v = in_10 % to_base;
+        ans.push_front(v);
+        in_10 /= to_base;
+    }
+
+    Ok(if let 0 = ans.len() {
+        vec![0]
+    } else {
+        ans.into()
+    })
 }
